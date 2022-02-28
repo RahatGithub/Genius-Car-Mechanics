5e222d8c069077cb42e59e9a14f45ab88ef925fd
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-02-28 08:53:18 +0600

    Login page added

---
+++ src/Pages/Booking/Booking/Booking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+
+const Booking = () => {
+
+    const {serviceId} = useParams();
+
+    return (
+        <div>
+            <h2>Booking {serviceId}</h2>
+        </div>
+    );
+};
+
+export default Booking;
